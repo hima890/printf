e1e7eb15225e98df1f2f4383f301d75c796403af
hima890 <hfibrahim90@gmail.com>
2023-10-14 20:54:42 +0200

    Add the initializeStringBuffer.c file to the utilities folder, Update the header file

--- header/main.h
+++ header/main.h
@@ -1,6 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
-/*Header files goes here*/
+/* Header files goes here */
+
+/* Defind morcos goes here*/
+#define BUFFER_SIZE 1024
+
+
+/*Functions prototypes goes here */
 int _printf(const char *format, ...);
+char *initializeStringBuffer(int buffer_size);
 
 #endif
---
+++ utilities/initializeStringBuffer.c
@@ -0,0 +1,51 @@
+#include "../header/main.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * initializeStringBuffer - Initializes a character buffer of a specified size.
+ *
+ * @buffer_size: The size of the character buffer to allocate.
+ *
+ * Description:
+ *   This function allocates a memory block for a character buffer of the given
+ *   size and initializes it with all elements set to '\0'. It returns a pointer
+ *   to the allocated memory.
+ *
+ *   If the provided buffer_size is less than or equal to 0, the function
+ *   returns NULL and displays an error message.
+ *
+ *   The caller is responsible for freeing the allocated memory when it is no
+ *   longer needed.
+ *
+ * Return: A pointer to the initialized character buffer, or NULL on failure.
+*/
+
+char *initializeStringBuffer(int buffer_size)
+{
+    char *buffer_pionter;/* Pointer to the memory block */
+
+    if (buffer_size <= 0) {
+        /* Handle invalid buffer size */
+        printf("Error invalid buffer size\n");
+        return NULL;
+    }
+
+    /* Allocatedand signed the memory to the pointer */
+    buffer_pionter = (char *)malloc(sizeof(char) * buffer_size);
+    if (buffer_pionter == NULL)
+    {
+        /* Memory allocation failed */
+        return (NULL);
+    }
+
+    /* Set the memory block to 0 */
+    memset(buffer_pionter, 0, buffer_size);
+    /* Return the memory pionter */
+    return (buffer_pionter);
+
+    /* Dont forget to free the pionter */
+
+}
+
