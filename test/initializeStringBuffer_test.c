79cdf1665ed3dab241140be6b82e905834d46121
hima890 <hfibrahim90@gmail.com>
2023-10-14 21:54:54 +0200

    Add the initializeStringBuffer_test.c to the test folder

---
+++ test/initializeStringBuffer_test.c
@@ -0,0 +1,39 @@
+/* Header files goes here */
+#include "header/main.h"
+
+
+/**
+ * main - Entry point for testing the initializeStringBuffer function.
+ *
+ * Description:
+ *   This function serves as a test case for the `initializeStringBuffer` function.
+ *   It allocates memory for a character buffer using `initializeStringBuffer`
+ *   and checks whether the buffer is successfully allocated and initialized with
+ *   '0'. It then prints a message indicating the result of the test.
+ *
+ *   If the buffer is successfully allocated, it prints a message indicating
+ *   that the buffer is allocated and initialized. Otherwise, it prints an error
+ *   message.
+ *
+ * Return: 0 (Always)
+*/
+
+int main(void)
+{
+    /* code */
+    char *string_buffer;
+    string_buffer = initializeStringBuffer(BUFFER_SIZE);
+
+    if (string_buffer != NULL)
+    {
+        /* code */
+        printf("The buffer is allocated and initialized with '0'.\n");
+    }
+    else
+    {
+        printf("An Error Occurred!!\n");
+    }
+
+    free(string_buffer);
+    return (0);
+}
