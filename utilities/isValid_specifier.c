d36d467820197f277733e1e5262ed27dcd99b5b8
hima890 <hfibrahim90@gmail.com>
2023-10-15 19:58:10 +0200

    Update the isValid_specifier function file in utilities folder and printf_test folder

--- utilities/isValid_specifier.c
+++ utilities/isValid_specifier.c
@@ -2,9 +2,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include "../main.h"
 
-int *isValid_specifier(char input_string_specifier)
+bool containsStringSpecifier(const char *input)
 {
-    return (1);
+    int loopIndex;
+    int length;
+    if (input == NULL)
+    {
+        /*to handle null input*/
+        
+        return (false);
+    }
+
+    length = strlen(input);
+    for (loopIndex = 0; loopIndex < length; loopIndex++)
+    {
+        if (input[loopIndex] == '%')
+        {
+            if ((loopIndex + 1) < length)
+            {
+                char nextChar = input[loopIndex + 1];
+                if (nextChar == 's' || nextChar == 'd' || nextChar == 'f' || nextChar == 'c')
+                {
+                    /* Check for known string specifiers like %s, %d, %f, %c, etc.*/
+                    return (true);
+                }
+            }
+        }
+    }
+
+    /*normal printing goes here*/
+    return (false);
 }
