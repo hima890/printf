bfc21f4130c08823fd02f650fbfe53acb74a856a
hima890 <hfibrahim90@gmail.com>
2023-10-15 12:01:45 +0200

    Add all the main printf def.functios with a place holder intend of the full logic

---
+++ utilities/findTheCorresponding_argiment.c
@@ -0,0 +1,10 @@
+/* Header files goes here */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../main.h"
+
+void *findTheCorresponding_argiment(char input_string_specifier, va_list input_string_arg)
+{
+    printf("findTheCorresponding_argiment\n");
+}
