c0c12b58cb7b9641f6ab40c4d23c470d2833f696
hima890 <hfibrahim90@gmail.com>
2023-10-19 01:14:28 +0200

    Update and test

--- utilities/main.c
+++ utilities/main.c
@@ -9,31 +9,29 @@
  */
 int main(void)
 {
-    /*
+
     int len;
     int len2;
-    unsigned int ui;
-    void *addr;
-
+    
+    int a = printf("%");
     len = _printf("Let's try to printf a simple sentence.\n");
     len2 = printf("Let's try to printf a simple sentence.\n");
-    ui = (unsigned int)INT_MAX + 1024;
-    addr = (void *)0x7ffe637541f0;
+    
     _printf("Length:[%d, %i]\n", len, len);
     printf("Length:[%d, %i]\n", len2, len2);
-    */
 
-   /*
+
+
     _printf("Negative:[%i]\n", -110);
     printf("Negative:[%d]\n", -762534);
     
 
    printf("%s", NULL);
    _printf("%s", NULL);
-    */
-   int a = printf(NULL);
-   printf("%d", a);
-   _printf(NULL);
+  
+   printf("%d\n", a);
+   printf("%wtytt\n");
+   _printf("%wety\n");
 /*
     _printf("Unsigned:[%u]\n", ui);
     printf("Unsigned:[%u]\n", ui);
